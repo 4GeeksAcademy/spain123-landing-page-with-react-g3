096c28a93d7d14b1b4691028938927c517fb3441
EdgardoSalazar <82413057+EdgardoSalazar@users.noreply.github.com>
2025-10-31 08:33:43 +0000

    Dejo el Home como el inicio

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import { Navbar } from "./Navbar";
+
 
 //create your first component
 const Home = () => {
